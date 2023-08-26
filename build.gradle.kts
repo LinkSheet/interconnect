29ae7288610bbc82bd1fabe7e8bccd25284c7df6
1fexd <PGFvy85nC@protonmail.com>
2023-08-26 15:23:50 +0200

    fix: Dependencies, update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("net.nemerosa.versioning") version "3.0.0" apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("net.nemerosa.versioning") apply false
 }
