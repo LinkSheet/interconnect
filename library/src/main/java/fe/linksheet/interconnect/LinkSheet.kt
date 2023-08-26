c2f10490a0c40d0738db1056c51085ea94fd5d46
1fexd <PGFvy85nC@protonmail.com>
2023-08-26 18:32:22 +0200

    chore: Refactoring to avoid querying system multiple times

--- library/src/main/java/fe/linksheet/interconnect/LinkSheet.kt
+++ library/src/main/java/fe/linksheet/interconnect/LinkSheet.kt
@@ -3,78 +3,32 @@ package fe.linksheet.interconnect
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.IBinder
 import androidx.core.content.ContextCompat
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-/**
- * A collection of utilities for interacting with
- * LinkSheet from external clients.
- */
-object LinkSheet {
-    const val INTERCONNECT_COMPONENT = "fe.linksheet.InterconnectService"
+class LinkSheet(
+    private val packageName: String,
+    private val interconnectComponentName: ComponentName?
+) {
+    val supportsInterconnect = interconnectComponentName != null
 
     /**
-     * Check if LinkSheet (release, nightly or debug) is installed.
-     */
-    fun Context.isLinkSheetInstalled(): Boolean {
-        return getInstalledPackageName() != null
-    }
-
-    /**
-     * Get the installed package name, if any.
-     * If multiple LinkSheet types (release/nightly/debug) or flavors (pro, foss, legacy) are installed,
-     * this will return pro > foss > legacy, each with the build types release > nightly > debug
-     * If LinkSheet is not installed, this returns null.
-     */
-    fun Context.getInstalledPackageName(): String? {
-        return LinkSheetPackageName.POSSIBLE_PACKAGE_NAMES.firstOrNull {
-            try {
-                @Suppress("DEPRECATION")
-                packageManager.getApplicationInfo(it, 0)
-                true
-            } catch (e: PackageManager.NameNotFoundException) {
-                false
-            }
-        }
-    }
-
-    fun Context.supportsInterconnect(interconnectComponentName: String = INTERCONNECT_COMPONENT): Boolean {
-        val installedPackage = getInstalledPackageName() ?: return false
-
-        return try {
-            packageManager.getServiceInfo(
-                ComponentName(
-                    installedPackage,
-                    interconnectComponentName
-                ), 0
-            )
-            true
-        } catch (e: PackageManager.NameNotFoundException) {
-            false
-        }
-    }
-
-    /**
-     * A convenience function for binding the interconnect service.
+     * A convenience function for binding the Interconnect service.
      *
      * If multiple LinkSheet types (release/nightly/debug) or flavors (pro, foss, legacy) are installed,
      * this will bind to pro > foss > legacy, each with the build types release > nightly > debug
      * If LinkSheet is not installed, this returns null.
      */
-    fun Context.bindService(
-        interconnectComponentName: String = INTERCONNECT_COMPONENT,
-        onBound: (LinkSheetServiceConnection) -> Unit,
-    ) {
-        if (!isLinkSheetInstalled()) {
-            throw IllegalStateException("LinkSheet is not installed!")
+    fun Context.bindService(onBound: (LinkSheetServiceConnection) -> Unit) {
+        if (!supportsInterconnect) {
+            throw IllegalStateException("Installed LinkSheet version does not support Interconnect")
         }
 
         val intent = Intent(Intent.ACTION_VIEW).apply {
-            `package` = getInstalledPackageName()
-            component = ComponentName(`package`!!, interconnectComponentName)
+            `package` = this@LinkSheet.packageName
+            component = interconnectComponentName
         }
 
         val connection = object : LinkSheetServiceConnection() {
@@ -92,19 +46,16 @@ object LinkSheet {
         bindService(intent, connection, Context.BIND_AUTO_CREATE)
     }
 
+
     /**
-     * A convenience function for binding the interconnect service.
+     * A convenience function for binding the Interconnect service.
      *
-     * If multiple LinkSheet versions are installed (release/nightly/debug), this will bind to
-     * release > nightly > debug
+     * If multiple LinkSheet types (release/nightly/debug) or flavors (pro, foss, legacy) are installed,
+     * this will bind to pro > foss > legacy, each with the build types release > nightly > debug
      */
-    suspend fun Context.bindService(
-        interconnectComponentName: String = INTERCONNECT_COMPONENT,
-    ): LinkSheetServiceConnection {
+    suspend fun Context.bindService(): LinkSheetServiceConnection {
         return suspendCoroutine { continuation ->
-            bindService(interconnectComponentName) {
-                continuation.resume(it)
-            }
+            bindService { continuation.resume(it) }
         }
     }
 }
---
+++ library/src/main/java/fe/linksheet/interconnect/LinkSheetConnector.kt
@@ -0,0 +1,67 @@
+package fe.linksheet.interconnect
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.content.pm.ServiceInfo
+
+/**
+ * A utility to check if LinkSheet is installed and supports Interconnect
+ */
+object LinkSheetConnector {
+    const val INTERCONNECT_COMPONENT = "fe.linksheet.InterconnectService"
+    const val BASE_PACKAGE_NAME = "fe.linksheet"
+
+    fun getSpecificTypePackageName(flavor: BuildFlavor, type: BuildType): String {
+        return apply(apply(BASE_PACKAGE_NAME, flavor.suffix), type.suffix)
+    }
+
+    private fun apply(packageName: String, suffix: String?): String {
+        return if (suffix != null) "${packageName}.${suffix}" else packageName
+    }
+
+    enum class BuildType(val suffix: String? = null) {
+        Release, Debug("debug"), Nightly("nightly")
+    }
+
+    enum class BuildFlavor(val suffix: String? = null) {
+        Pro("pro"), Foss("foss"), Legacy
+    }
+
+    val POSSIBLE_PACKAGE_NAMES = BuildFlavor.values().flatMap { flavor ->
+        val flavorPackageName = apply(BASE_PACKAGE_NAME, flavor.suffix)
+        BuildType.values().map { type -> apply(flavorPackageName, type.suffix) }
+    }
+
+    /**
+     * Get a [LinkSheet] instance if LinkSheet is installed
+     * If multiple LinkSheet types (release/nightly/debug) or flavors (pro, foss, legacy) are installed,
+     * this will return pro > foss > legacy, each with the build types release > nightly > debug
+     * If LinkSheet is not installed, this returns null.
+     */
+    fun Context.getLinkSheet(interconnectComponent: String = INTERCONNECT_COMPONENT): LinkSheet? {
+        return POSSIBLE_PACKAGE_NAMES.firstNotNullOfOrNull {
+            getLinkSheet(it, interconnectComponent)
+        }
+    }
+
+    fun Context.getLinkSheet(
+        packageName: String,
+        interconnectComponent: String = INTERCONNECT_COMPONENT
+    ): LinkSheet? {
+        packageManager.getApplicationInfoOrNull(packageName) ?: return null
+
+        val componentName = ComponentName(packageName, interconnectComponent)
+        val hasInterconnect = packageManager.getServiceInfoOrNull(componentName)
+        return LinkSheet(packageName, if (hasInterconnect != null) componentName else null)
+    }
+
+    private fun PackageManager.getApplicationInfoOrNull(packageName: String): ApplicationInfo? {
+        return runCatching { getApplicationInfo(packageName, 0) }.getOrNull()
+    }
+
+    private fun PackageManager.getServiceInfoOrNull(componentName: ComponentName): ServiceInfo? {
+        return runCatching { getServiceInfo(componentName, 0) }.getOrNull()
+    }
+}
